a2746a010acff20c33154d5fc1b1e4ea4ea3f94b
Satyam <satyamchauhan709@gmail.com>
2024-08-06 22:38:40 +0530

    daily 6-08-24 improved

--- src/MinPush_6_8_24.java
+++ src/MinPush_6_8_24.java
@@ -1,39 +1,27 @@
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class MinPush_6_8_24 {
-
-    private record Freq(char ch,int freq) implements Comparable<Freq>{
-        @Override
-        public int compareTo(Freq o) {
-            return Integer.compare(this.freq,o.freq);
-        }
-    }
     public int minimumPushes(String word) {
         int[] freq = new int[26];
-        for (char i : word.toCharArray()){
-            freq[i-97]++;
+        for (char i : word.toCharArray()) {
+            freq[i - 97]++;
         }
 
-        PriorityQueue<Freq> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Arrays.sort(freq);
 
-        for (int i = 0; i < 26; i++) {
-            if (freq[i]!=0) {
-                System.out.println((char) (i + 97) + " -> " + freq[i]);
-                pq.add(new Freq((char) (i + 97),freq[i]));
-            }
-        }
-        System.out.println(pq);
-        int i =8,minSum =0;
-        while(!pq.isEmpty()){
-            minSum += i/8 * freq[pq.poll().ch - 97];
+        int i = 8, minSum = 0, f, k = 26;
+        while ((f = freq[--k]) != 0) {
+            minSum += i / 8 * f;
             i++;
         }
         return minSum;
     }
 
     public static void main(String[] args) {
-        String s  ="abzaqsqcyrbzsrvamylmyxdjl";
+        String s = "abzaqsqcyrbzsrvamylmyxdjl";
         MinPush_6_8_24 obj = new MinPush_6_8_24();
         System.out.println(obj.minimumPushes(s));
     }
