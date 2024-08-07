38eb9d7896ad383680f766007a1ac95aa470301d
Satyam <satyamchauhan709@gmail.com>
2024-08-07 19:26:53 +0530

    done 7-08-24 daily improved slightly use array for more performance

--- src/NumberToWords.java
+++ src/NumberToWords.java
@@ -3,56 +3,43 @@ import java.util.HashMap;
 public class NumberToWords {
     public String numberToWords(int num) {
 
-        StringBuilder[] suf = new StringBuilder[4];
+        if (num == 0)
+            return "Zero";
 
-        for (int i = 0; i < 4; i++) {
-            suf[i] = new StringBuilder();
-        }
+        StringBuilder suf = new StringBuilder();
 
         HashMap<Integer, String> numberWordsMap = new HashMap<>();
         constructMap(numberWordsMap);
 
-        int n = num, sufix = 0;
+        int n = num, sufix = 3;
 
         while (n > 0) {
-            appendSuffix(n % 1000, numberWordsMap, suf[sufix], sufix);
-            n /= 1000;
-            sufix++;
+            int factor = (int) Math.pow(1000, sufix);
+            appendSuffix(n / factor, numberWordsMap, suf, sufix);
+            n %= factor;
+            sufix--;
         }
-
-        for (int i = 2; i >= 0; i--) {
-            suf[3].append(suf[i]);
-        }
-
-        String s =suf[3].toString();
-        return s.substring(0,s.length()-1);
+        String s = suf.toString();
+        return s.substring(0, s.length() - 1);
     }
 
     private void appendSuffix(int n, HashMap<Integer, String> numberWordsMap, StringBuilder suf, int sufix) {
 
-        if (n / 1000 > 0)
-            throw new IllegalArgumentException("Greater number than 3 digits");
+        if (n == 0) return;
 
-        if (n / 100 != 0) {
+        if (n >= 100) {
             suf.append(numberWordsMap.get(n / 100)).append(" ").append(numberWordsMap.get(100)).append(" ");
-        }
-        n %= 100;
+            n %= 100;
 
-        if (n < 20 && n >= 10) {
-            suf.append(numberWordsMap.get(n)).append(" ");
-        } else {
-            if (n / 10 != 0) {
-                suf.append(numberWordsMap.get(10 * (n / 10))).append(" ");
-            }
+        }
+        if (n >= 20) {
+            suf.append(numberWordsMap.get(10 * (n / 10))).append(" ");
             n %= 10;
-            if (n != 0) {
-                suf.append(numberWordsMap.get(n)).append(" ");
-            }
         }
+        if (n > 0)
+            suf.append(numberWordsMap.get(n)).append(" ");
 
-        // add suffix if !empty
-        if (suf.isEmpty())return;
-
+        // add suffix
         if (sufix == 1) {
             // thousands
             suf.append(numberWordsMap.get(101)).append(" ");
@@ -66,7 +53,6 @@ public class NumberToWords {
     }
 
     private void constructMap(HashMap<Integer, String> numberWordsMap) {
-        numberWordsMap.put(0, "Zero");
         numberWordsMap.put(1, "One");
         numberWordsMap.put(2, "Two");
         numberWordsMap.put(3, "Three");
@@ -101,7 +87,7 @@ public class NumberToWords {
     }
 
     public static void main(String[] args) {
-        int n = 314490121;
+        int n = 1000010;
         NumberToWords obj = new NumberToWords();
         System.out.println(obj.numberToWords(n));
     }
