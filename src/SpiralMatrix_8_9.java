93d7b2d2caa16f4029c47b91556da066cda6d010
Satyam <satyamchauhan709@gmail.com>
2024-08-08 13:38:25 +0530

    spiral matrix daily 8-8-24 optimised but...

--- src/SpiralMatrix_8_9.java
+++ src/SpiralMatrix_8_9.java
@@ -8,48 +8,51 @@ public class SpiralMatrix_8_9 {
         int k = 0;
         outer:
         while (k != rows * cols) {
-            for (int i = lc; i <= uc; i++) {
-                if (isInvalid(lr, rows, i, cols))
-                    continue;
+            // Move right
+            for (int i = Math.max(lc, 0); i <= Math.min(cols - 1, uc); i++) {
+                if (isInvalid(lr, rows)) break;
                 if (k == rows * cols) break outer;
                 out[k++] = new int[]{lr, i};
             }
             uc++;
 
-            for (int i = lr; i <= ur; i++) {
-                if (isInvalid(i, rows, uc, cols)) continue;
+            // Move down
+            for (int i = Math.max(0, lr); i <= Math.min(rows - 1, ur); i++) {
+                if (isInvalid(uc, cols)) break;
                 if (k == rows * cols) break outer;
                 out[k++] = new int[]{i, uc};
             }
             ur++;
 
-            for (int i = uc; i >= lc; i--) {
-                if (isInvalid(ur,rows, i, cols)) continue;
+            // Move left
+            for (int i = Math.min(cols - 1, uc); i >= Math.max(0, lc); i--) {
+                if (isInvalid(ur, rows)) break;
                 if (k == rows * cols) break outer;
                 out[k++] = new int[]{ur, i};
             }
             lc--;
 
-            for (int i = ur; i >= lr; i--) {
-                if (isInvalid(i, rows, lc, cols)) continue;
+            // Move up
+            for (int i = Math.min(rows - 1, ur); i >= Math.max(0, lr); i--) {
+                if (isInvalid(lc, cols)) break;
                 if (k == rows * cols) break outer;
                 out[k++] = new int[]{i, lc};
             }
             lr--;
-
-        }
-        for (int[] i : out) {
-            System.out.println(Arrays.toString(i));
         }
         return out;
     }
 
-    private boolean isInvalid(int i, int rows, int j, int cols) {
-        return i < 0 || j < 0 || j >= cols || i >= rows;
+    private boolean isInvalid(int i, int limit) {
+        return i < 0 || i >= limit;
     }
 
     public static void main(String[] args) {
         SpiralMatrix_8_9 obj = new SpiralMatrix_8_9();
-        obj.spiralMatrixIII(5, 6, 1, 4);
+        int[][] out = obj.spiralMatrixIII(5, 6, 1, 4);
+        int k=0;
+        for (int[] i : out) {
+            System.out.println(k++ +" "+Arrays.toString(i));
+        }
     }
 }
