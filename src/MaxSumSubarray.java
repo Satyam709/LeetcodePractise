687a7e6eb982ec6a9a06c1a9f95edd80cb690cb9
Satyam <satyamchauhan709@gmail.com>
2024-08-16 00:56:59 +0530

    did MaxSumSubarray

---
+++ src/MaxSumSubarray.java
@@ -0,0 +1,17 @@
+public class MaxSumSubarray {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0], crntSum = 0;
+        for (int num : nums) {
+            crntSum += num;
+            maxSum = Math.max(crntSum, maxSum);
+            if (crntSum < 0) crntSum = 0;
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,4,-1,7,8};
+        MaxSumSubarray obj = new MaxSumSubarray();
+        System.out.println(obj.maxSubArray(nums));
+    }
+}
