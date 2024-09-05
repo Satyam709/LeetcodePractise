b37598f91798cfcfc3f4336184f53dfd09f8a4bc
Satyam <satyamchauhan709@gmail.com>
2024-09-05 11:36:18 +0530

    done daily 5-9 missing no 2

--- src/MissObservations.java
+++ src/MissObservations.java
@@ -30,6 +30,28 @@ public class MissObservations {
         return out;
     }
 
+
+    public int[] missingRolls2(int[] rolls, int mean, int n) {
+
+        int rem = 0;
+        for (int i : rolls) rem += i;
+        rem = (rolls.length + n) * mean - rem;
+        if (rem > 6 * n || rem < n) return new int[]{};
+
+        int[] out = new int[n];
+        int avg = rem/n;
+        Arrays.fill(out,avg);
+        rem%=n;
+
+        int i = 0;
+        while (rem>0 && i<n) {
+            out[i++]++;
+            rem--;
+        }
+        return out;
+    }
+
+
     public static void main(String[] args) {
         int[] rolls = {6,1,5,2};
         int mean = 4;
