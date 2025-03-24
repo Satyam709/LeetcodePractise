b5db68d0ad440c6576df6044ae9193922814b322
satyam709 <satyamchauhan709@gmail.com>
2025-03-24 18:29:59 +0530

    improved 25-3-2025

--- src/Daily_24_03_2025.java
+++ src/Daily_24_03_2025.java
@@ -6,30 +6,17 @@ public class Daily_24_03_2025 {
     public static int countDays(int days, int[][] meetings) {
         Arrays.sort(meetings, Comparator.comparingInt(a->a[0]));
         int count = 0 ;
-        ArrayList<int[]> li = new ArrayList<>();
         int lw = meetings[0][0], up = meetings[0][1];
-        for (int i = 1; i < meetings.length; i++) {
+        for (int i = 1; i < meetings.length && lw <= days; i++) {
             if (meetings[i][0]>up){
-                li.add(new int[]{lw,up});
+                count += Math.min(up, days) - lw + 1;
                 lw = meetings[i][0];
                 up = meetings[i][1];
             } else {
                 up = Math.max(up,meetings[i][1]);
             }
         }
-        li.add(new int[]{lw,up});
-        System.out.println("original array "+ Arrays.deepToString(meetings));
-        System.out.print("reworked array : ");
-        for (int[] i : li){
-            System.out.print(Arrays.toString(i) +" ");
-        }
-        System.out.println();
-        for (int[] crntMeet : li) {
-            int from = crntMeet[0], to = crntMeet[1];
-
-            if (from > days) break;
-            count += Math.min(to, days) - from + 1;
-        }
+        count += lw>days?0:Math.min(up, days) - lw + 1;
         return days - count;
     }
     public static void main(String[] args) {
