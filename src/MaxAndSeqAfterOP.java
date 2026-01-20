c59278c0af8e79bc14c472e873a1bda91713b948
satyam709 <satyamchauhan709@gmail.com>
2026-01-21 00:29:57 +0530

    check2

--- src/MaxAndSeqAfterOP.java
+++ src/MaxAndSeqAfterOP.java
@@ -27,14 +27,12 @@ public class MaxAndSeqAfterOP {
         for (int i = 30; i >= 0 && remOp > 0; i--) {
             // {val,op}
             int target = 1 << i;
-            System.out.println("bit = " + i + ", target= " + target);
             PriorityQueue<int[]> pq = new PriorityQueue<>(new comaprator());
             for (int j = 0; j < nums2.length; j++) {
                 int opReq = Math.max(0, target - nums2[j]); // if nums[j] > 0 target than the bit is already
                                                             // set take it as zero
                 if (pq.size() == m) {
-                    int[] polled = pq.poll();
-                    System.out.println("size reached polled = " + Arrays.toString(polled));
+                    pq.poll();
                 }
                 pq.add(new int[] { nums2[j], opReq });
             }
@@ -47,13 +45,8 @@ public class MaxAndSeqAfterOP {
                 int[] polled = pq.poll();
                 sum += polled[1];
                 newAr[l++] = polled[0] + polled[1]; // perform the op
-                System.out.print(Arrays.toString(polled) + " ");
             }
-            System.out.println();
-
-            System.out.println("remOP= "+remOp);
             if (sum <= remOp) {
-                System.out.println("found a valid subset! yay!");
                 // this subset is possible
 
                 // now we will just look on this
@@ -61,7 +54,6 @@ public class MaxAndSeqAfterOP {
 
                 // account this in and
                 and |= target;
-                System.out.println("crnt and = " + and);
 
                 // update rem op
                 remOp -= sum;
@@ -75,7 +67,6 @@ public class MaxAndSeqAfterOP {
                     nums2[j] -= target;
                 }
             }
-            System.out.println("updated NUMS2 = " + Arrays.toString(nums2));
         }
         return and;
     }
@@ -84,7 +75,6 @@ public class MaxAndSeqAfterOP {
         int[] nums = { 3, 1, 2 };
         int k = 8, m = 2;
         System.out.println("out1 = " + maximumAND(nums, k, m));
-
 
         System.out.println("\n");
 
